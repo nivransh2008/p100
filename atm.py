c22471510193c6e908cfee65050aa2c0a55b93d1
nivransh2008 <69744641+nivransh2008@users.noreply.github.com>
2021-07-21 15:21:25 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,10 @@
+class Atm(object): 
+    def __init__(self, atmcardnumber, pin):
+        self.atmcardnumber = atmcardnumber 
+        self.pin = pin 
+         
+def cashwithdrawl () :
+    print("cash can be with drawn") 
+
+def balanceenquiry () : 
+    print("Find your balance enquiry") 
